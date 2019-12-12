90fa40698549bac69eaafe35b7409a3f03947ba4
wwwjjjnnn <1194348056@qq.com>
2019-12-12 09:48:18 +0800

    update bert_crf.

--- submit/main.py
+++ submit/main.py
@@ -38,7 +38,7 @@ def main():
     项目的超参
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--EPOCHS", default=10, type=int, help="train epochs")
+    parser.add_argument("-e", "--EPOCHS", default=50, type=int, help="train epochs")
     parser.add_argument("-b", "--BATCH", default=8, type=int, help="batch size")
     args = parser.parse_args()
 
--- submit/predict.py
+++ submit/predict.py
@@ -3,7 +3,7 @@
 实现模型的调用
 """
 from flyai.dataset import Dataset
-from model import Model
+from .model import Model
 
 data = Dataset()
 model = Model(data)
